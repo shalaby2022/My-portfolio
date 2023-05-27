ad564e8b24f2d169f6ad7c09eba8b401e643cfdc
shalaby2022 <ahmedshalaby201495@gmail.com>
2023-05-27 14:40:58 +0300

    editing navigation bar

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -30,7 +30,7 @@ const NavigationBar = () => {
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="/home">
+        <Navbar.Brand href="#home">
           <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
