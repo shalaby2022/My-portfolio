b586ee1cb7656935ed433634e19f7dcb5022e28b
shalaby2022 <ahmedshalaby201495@gmail.com>
2023-05-27 14:34:03 +0300

    editing navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import NavigatBar from "./components/NavigationBar";
 import Master from "./pages/Landing";
 
 const App = () => {
